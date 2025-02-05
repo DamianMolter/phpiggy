9a4326b1f52d84f4c99db1d54ea024fec133ee23
DamianMolter <163694119+DamianMolter@users.noreply.github.com>
2025-02-06 00:09:26 +0100

    Added Controller and began Template Engine

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -2,10 +2,15 @@
 
 declare(strict_types=1);
 
-include __DIR__ . "/../../vendor/autoload.php";
+require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\HomeController;
 
 $app = new App();
+
+$app->get('/', [HomeController::class, 'home']);
+
+$app->run();
 
 return $app;
