9d8e1b776031097848f6f18d3a77f68d51f27eb0
DamianMolter <163694119+DamianMolter@users.noreply.github.com>
2025-02-09 18:07:12 +0100

    Finished Container

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -6,11 +6,12 @@ require __DIR__ . "/../../vendor/autoload.php";
 
 use App\Config\Paths;
 use Framework\App;
-use function App\Config\registerRoutes;
+use function App\Config\{registerRoutes, registerMiddleware};
 
 $app = new App(Paths::SOURCE . "App/Config/container-definitions.php");
 
 registerRoutes($app);
+registerMiddleware($app);
 
 $app->run();
 
