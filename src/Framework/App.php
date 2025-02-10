66ae4a779d7a6dd323183b00bce55b60ff432d3d
DamianMolter <163694119+DamianMolter@users.noreply.github.com>
2025-02-10 16:21:07 +0100

    Validator under construction

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -33,6 +33,11 @@ class App
             $this->router->add('GET', $path, $controller);
       }
 
+      public function post($path, array $controller)
+      {
+            $this->router->add('POST', $path, $controller);
+      }
+
       public function addMiddleware(string $middleware)
       {
             $this->router->addMiddleware($middleware);
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Framework;
 
-use function PHPSTORM_META\map;
-
 class Router
 {
 
