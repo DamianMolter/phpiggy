da78709a632b6d1303fc094a90941dd802c1e0d2
DamianMolter <163694119+DamianMolter@users.noreply.github.com>
2025-02-14 16:02:43 +0100

    Finished Validator

--- src/Framework/Validator.php
+++ src/Framework/Validator.php
@@ -6,6 +6,8 @@ declare(strict_types=1);
 namespace Framework;
 
 use Framework\Contracts\RuleInterface;
+use Framework\Exceptions\ValidationException;
+use RuntimeException;
 
 class Validator
 {
@@ -20,19 +22,27 @@ class Validator
             $errors = [];
             foreach ($fields as $fieldName => $rules) {
                   foreach ($rules as $rule) {
+
+                        $ruleParams = [];
+
+                        if (str_contains($rule, ':')) {
+                              [$rule, $ruleParams] = explode(':', $rule);
+                              $ruleParams = explode(',', $ruleParams);
+                        }
+
                         $ruleValidator = $this->rules[$rule];
-                        if ($ruleValidator->validate($formData, $fieldName, [])) {
+                        if ($ruleValidator->validate($formData, $fieldName, $ruleParams)) {
                               continue;
                         }
                         $errors[$fieldName][] = $ruleValidator->getMessage(
                               $formData,
                               $fieldName,
-                              []
+                              $ruleParams
                         );
                   }
             }
             if (count($errors)) {
-                  dd($errors);
+                  throw new ValidationException($errors);
             }
       }
 }
